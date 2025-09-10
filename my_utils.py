9a6b191116462580ea2abe66d1fb261c666b4f5d
Lucinda_Shaffer <lucinda.shaffer@colorado.edu>
2025-09-09 23:20:59 -0600

    get_column implemented | index fixed | fires column compiled

--- my_utils.py
+++ my_utils.py
@@ -4,9 +4,9 @@ def get_column(file_name, query_column, query_value, result_column):
         results = []
         for line in data [1:]:  # Skip header
             columns = line.strip().split(',')
-            if columns[query_column] == query_value:
-                results.append(columns[result_column])
+            if columns[query_column-1] == query_value:
+                results.append(columns[result_column-1])
         return results
-print (get_column("Agrofood_co2_emission.csv", 0, "Zimbabwe", 2))
+#print (get_column("Agrofood_co2_emission.csv", 0, "Zimbabwe", 2))
 
 
