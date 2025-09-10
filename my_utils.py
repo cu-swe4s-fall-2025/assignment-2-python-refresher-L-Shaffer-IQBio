63aa1ac0f9b3b0e1f03b119f5a5ce5c0b3591a67
Lucinda_Shaffer <lucinda.shaffer@colorado.edu>
2025-09-09 23:05:15 -0600

    get column function implemented

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,12 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    with open(file_name, 'r') as file:
+        data = file.readlines()
+        results = []
+        for line in data [1:]:  # Skip header
+            columns = line.strip().split(',')
+            if columns[query_column] == query_value:
+                results.append(columns[result_column])
+        return results
+print (get_column("Agrofood_co2_emission.csv", 0, "Zimbabwe", 2))
+
+
